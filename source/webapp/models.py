211df36b7992137719a10b7c8c1fd6de5a9d3969
Said Saliev <repsaid@gmail.com>
2023-02-20 12:03:30 +0600

    payment model

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Cумма')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата начала")
